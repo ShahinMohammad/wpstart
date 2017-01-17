fc8004b4a67c1d85110e779f97de056db2847f50
shahinMohammad <shahinmohammad24rb@gmail.comgit config --global user.email shahinmohammad24rb@gmail.com>
2017-01-17 23:32:41 +0600

    Bootstrap,navwalker,fontwsome add here

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package wpstart
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wpstart' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'wpstart' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'wpstart' ), 'wpstart', '<a href="https://automattic.com/" rel="designer">wpstart</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
